15439ff3a245034ffd5261691b8f3ace00d98b06
Ana Amante <Ana.Amante@endava.com>
2021-03-05 18:08:43 -0300

    added polymorphism ex

---
+++ src/inheritanceandpolymorphism/Language.java
@@ -0,0 +1,35 @@
+package inheritanceandpolymorphism;
+/*Languages, just like people, often have families. You can even map language lineage a bit like a family tree.
+
+Java can help us build a model to track inherited traits across families. In this case, we’ll focus on something that
+often varies between language families: word order — where the subject, verb, and object would go in a sentence.
+
+ */
+public class Language {
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+
+    public Language(String name, int numSpeakers, String regionsSpoken, String wordOrder) {
+        this.name = name;
+        this.numSpeakers = numSpeakers;
+        this.regionsSpoken = regionsSpoken;
+        this.wordOrder = wordOrder;
+    }
+
+    public void getInfo(){
+        System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken + ".");
+        System.out.println("The language follows the word order: " + this.wordOrder + ".");
+    }
+
+
+
+    public static void main(String[] args) {
+        Language mayan = new Mayan("Ki'che'", 2000);
+        Language mandarin = new SinoTibetan("Mandarin Chinese", 5558846);
+        Language burmese = new SinoTibetan("Burmese", 65253);
+
+        
+    }
+}
---
+++ src/inheritanceandpolymorphism/SinoTibetan.java
@@ -0,0 +1,12 @@
+package inheritanceandpolymorphism;
+
+public class SinoTibetan extends Language{
+
+    public SinoTibetan(String name, int numSpeakers) {
+        super(name, numSpeakers, "Asia", "subject-object-verb");
+        if(name.contains("Chinese")){
+            this.wordOrder = "subject-verb-object";
+        }
+    }
+}
+
